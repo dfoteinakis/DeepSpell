34e356b70ac65eea02fb0c66b608bc01d7309a92
Dimitris Foteinakis <dfoteinakis@gmail.com>
2019-04-01 11:48:16 +0300

    Fix compatibility issues

--- keras_spell.py
+++ keras_spell.py
@@ -59,7 +59,7 @@ CONFIG.validation_steps = 10
 CONFIG.number_of_iterations = 10
 #pylint:enable=attribute-defined-outside-init
 
-DIGEST = sha256(json.dumps(CONFIG.__dict__, sort_keys=True)).hexdigest()
+# DIGEST = sha256(json.dumps(CONFIG.__dict__, sort_keys=True)).hexdigest()
 
 # Parameters for the dataset
 MIN_INPUT_LEN = 5
@@ -84,9 +84,9 @@ SAVED_MODEL_FILE_NAME = os.path.join(DATA_FILES_FULL_PATH, "keras_spell_e{}.h5")
 # Some cleanup:
 NORMALIZE_WHITESPACE_REGEX = re.compile(r'[^\S\n]+', re.UNICODE) # match all whitespace except newlines
 RE_DASH_FILTER = re.compile(r'[\-\˗\֊\‐\‑\‒\–\—\⁻\₋\−\﹣\－]', re.UNICODE)
-RE_APOSTROPHE_FILTER = re.compile(r'&#39;|[ʼ՚＇‘’‛❛❜ߴߵ`‵´ˊˋ{}{}{}{}{}{}{}{}{}]'.format(unichr(768), unichr(769), unichr(832),
-                                                                                      unichr(833), unichr(2387), unichr(5151),
-                                                                                      unichr(5152), unichr(65344), unichr(8242)),
+RE_APOSTROPHE_FILTER = re.compile(r'&#39;|[ʼ՚＇‘’‛❛❜ߴߵ`‵´ˊˋ{}{}{}{}{}{}{}{}{}]'.format(chr(768), chr(769), chr(832),
+                                                                                      chr(833), chr(2387), chr(5151),
+                                                                                      chr(5152), chr(65344), chr(8242)),
                                   re.UNICODE)
 RE_LEFT_PARENTH_FILTER = re.compile(r'[\(\[\{\⁽\₍\❨\❪\﹙\（]', re.UNICODE)
 RE_RIGHT_PARENTH_FILTER = re.compile(r'[\)\]\}\⁾\₎\❩\❫\﹚\）]', re.UNICODE)
@@ -572,14 +572,14 @@ def train_speller(from_file=None):
     itarative_train(model)
 
 if __name__ == '__main__':
-#     download_the_news_data()
-#     uncompress_data()
-#     preprocesses_data_clean()
-#     preprocesses_data_analyze_chars()
-#     preprocesses_data_filter()
-#     preprocesses_split_lines() --- Choose this step or:
-#     preprocesses_split_lines2()
-#     preprocesses_split_lines4()
-#     preprocess_partition_data()
-#     train_speller(os.path.join(DATA_FILES_FULL_PATH, "keras_spell_e15.h5"))
-    train_speller()
+    download_the_news_data()
+    uncompress_data()
+    preprocesses_data_clean()
+    preprocesses_data_analyze_chars()
+    preprocesses_data_filter()
+    # preprocesses_split_lines() --- Choose this step or:
+    preprocesses_split_lines2()
+    # preprocesses_split_lines4()
+    preprocess_partition_data()
+    train_speller(os.path.join(DATA_FILES_FULL_PATH, "keras_spell_e15.h5"))
+    # train_speller()
